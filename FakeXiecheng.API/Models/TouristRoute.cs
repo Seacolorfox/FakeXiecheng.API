c83f526edc549680aa10a541277912007a26c74e
Seacolorfox Tokkio <33200403+Seacolorfox@users.noreply.github.com>
2021-04-12 22:46:19 +0800

    使用Entity Framework创建数据库

--- FakeXiecheng.API/Models/TouristRoute.cs
+++ FakeXiecheng.API/Models/TouristRoute.cs
@@ -2,23 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FakeXiecheng.API.Models
 {
     public class TouristRoute
     {
+        [Key]//表示为主键
         public Guid Id { get; set; }
+        [Required]//表示不为空
+        [MaxLength(100)]//长度限制
         public string Title { get; set; }
+        [Required]
+        [MaxLength(1500)]
         public string Description { get; set; }
+        [Column(TypeName ="decimal(18,2)")]//添加限定
         public decimal OriginalPrice { get; set; }
+        [Range(0.0,1.0)]
         public double? DiscountPresent { get; set; }
         public DateTime CreateTime { get; set; }
         public DateTime? UpdateTime { get; set; }
         public DateTime? DepartureTime { get; set; }
+        [MaxLength]//最大长度的限制
         public string Features { get; set; }
+        [MaxLength]
         public string Fees { get; set; }
+        [MaxLength]
         public string Notes { get; set; }
         //一条旅游路线可以有多张旅游图片，为1对N关系
-        public ICollection<TouristRoutePicture> TouristRoutePictures { get; set; }
+        public ICollection<TouristRoutePicture> TouristRoutePictures { get; set; } 
+            = new List<TouristRoutePicture>();//这一步是为了让代码更健壮避免一些未知错误
     }
 }
--- FakeXiecheng.API/Models/TouristRoutePicture.cs
+++ FakeXiecheng.API/Models/TouristRoutePicture.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +9,12 @@ namespace FakeXiecheng.API.Models
 {
     public class TouristRoutePicture
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]//让数据库自增
         public int Id { get; set; }
+        [MaxLength(100)]
         public string  Url { get; set; }
-        //与旅游路线的外键关系
+        [ForeignKey("TouristRouteId")]//与旅游路线的外键关系,Entity Framework 在映射数据库时会自动把每个模型的主键自动以类名加ID的形式做外键关联
         public Guid TouristRouteId { get; set; }
         //连接属性
         public TouristRoute TouristRoute { get; set; }
